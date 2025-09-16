05aafd274819ff38cdc8e328777448f2eee0747e
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2025-09-15 19:15:02 -0500

    Laboratorio 2 terminado

---
+++ Laboratorio 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //Ejemplo utilizando las variables de instancia de Clase.
+            client.FirstName = "Josemanuel";
+            client.LastName = "Sifontes";
+            client.Age = 23;
+            client.id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+    public class Client
+    {
+        //Declarando variables de instancia en clase.
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ Laboratorio 22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace laboratorio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asignando valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        //Declarando variable estatica.
+        public static int Valor;
+    }
+}
