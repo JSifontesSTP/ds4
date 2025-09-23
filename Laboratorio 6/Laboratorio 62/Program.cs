d414473a321fe450668c5bc06356db41db738fd8
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2025-09-22 20:07:21 -0500

    Finalizado laboratorio 6

---
+++ Laboratorio 6/Laboratorio 62/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program2
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("Digite el numero deseado");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+            Console.WriteLine(num);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("No ha introducido un dijito valido");
+            num = -1;
+            Console.WriteLine(num);
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("El numero es demasiado grande o demasiado pequeño para un Int16");
+            num = -1;
+            Console.WriteLine(num);
+        }
+        Console.WriteLine(num);
+    }
+}
---
+++ Laboratorio 6/Laboratorio 64/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program4
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso denegado - No cumple con el criterio de edad.");
+        }
+        else
+        {
+            Console.WriteLine("Acceso concedido");
+        }
+    }
+    static void Main(string[] args)
+    {
+        try
+        {
+            checkAge(15);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Se ha producido una excepción: " + e.Message);
+        }
+        
+    }
+}
